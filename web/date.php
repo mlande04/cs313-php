29e5c429377931038a3c967aada9a4986d43448b
Michelle Landen <lan18002@byui.edu>
2019-01-18 11:09:45 -0800

    added files

---
+++ web/date.php
@@ -0,0 +1,13 @@
+<html>
+<body>
+
+<?php
+	// display today's date and time
+    date_default_timezone_set("America/Los_Angeles");
+	$date=date("m/d/Y h:i:s a");
+	
+	echo $date;
+?>
+
+</html>
+</body>
